9b710ddb7edf0bdaff757a157fc788a94042e471
Randall Cox <rcoxwork@gmail.com>
2020-10-30 15:03:28 -0700

    Add local storage to save theme choice between user sessions

--- script.js
+++ script.js
@@ -37,12 +37,29 @@ function switchTheme(event) {
   // console.log(event.target.checked);
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+// console.log(currentTheme);
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
+
+
+
